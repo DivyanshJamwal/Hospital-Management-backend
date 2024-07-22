f3f14698107494ac87b64cb477f0f5a2e2f838a1
Coder-X <divyanshsingh1754@gmail.com>
2024-07-23 00:32:02 +0530

    changes

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,9 +1,14 @@
 const mongoose = require("mongoose")
 require("dotenv").config()
 
-MONGODB_CONN_STRING = process.env.MONGODB_URL
-
-mongoose.connect(MONGODB_CONN_STRING)
- .then(() => {
-    console.log("Connected to MongoDB");
-})
+mongoose.connect(process.env.MONGODB_URL.replace('localhost', '127.0.0.1'), {
+    serverSelectionTimeoutMS: 5000, // Increase server selection timeout
+    socketTimeoutMS: 45000, // Increase socket timeout
+    connectTimeoutMS: 30000, // Increase connect timeout
+  })
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((error) => {
+      console.error("MongoDB connection error:", error);
+    });
