9f119cfd6f2c8f3cf9dc11e6648e48eabb87637c
Coder-X <divyanshsingh1754@gmail.com>
2024-07-23 00:09:33 +0530

    minor changes

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,9 +1,13 @@
-const mongoose = require("mongoose")
-require("dotenv").config()
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-MONGODB_CONN_STRING = process.env.MONGODB_URL
+const MONGODB_CONN_STRING = process.env.MONGODB_URL;
 
-mongoose.connect(MONGODB_CONN_STRING)
- .then(() => {
-    console.log("Connected to MongoDB");
-})
+mongoose.connect(MONGODB_CONN_STRING);
+const conn = mongoose.connection;
+conn.on('error',(error)=>{
+    console.log('Connection failed...');
+});
+conn.once('open', () => {
+    console.log('Database connected...');   
+});
